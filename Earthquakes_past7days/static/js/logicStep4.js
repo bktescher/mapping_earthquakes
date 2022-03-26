7f61e58180982a6f3f48c93b504aa1d86c077b59
Brett <bnb@bnb.attlocal.net>
2022-03-26 12:26:41 -0400

    13.6.4 complete + adjustments

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -21,7 +21,16 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 let baseMaps = {
     "Streets" : streets,
     "Satellite Streets" : satellites
-  };
+};
+
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
 
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
@@ -30,8 +39,9 @@ let map = L.map('mapid', {
     layers: [streets]
 })
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
@@ -42,12 +52,32 @@ function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: "#ffae42",
+    fillColor: getColor(feature.properties.mag),
     color: "#000000",
-    radius: getRadius(),
+    radius: getRadius(feature.properties.mag),
     stroke: true,
     weight: 0.5
   };
+}
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
 }
 
 // This function determines the radius of the earthquake marker based on its magnitude.
@@ -67,6 +97,15 @@ function getRadius(magnitude) {
                 console.log(data);
                 return L.circleMarker(latlng);
             },
-    style: styleInfo
-        }).addTo(map);
+      // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+  // We create a popup for each circleMarker to display the magnitude and
+  //  location of the earthquake after the marker has been created and styled.
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+}
+        }).addTo(earthquakes);
+
+        //then we add the earthquake later to our map
+        earthquakes.addTo(map);
     });
